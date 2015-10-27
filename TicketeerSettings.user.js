5cb8c7a4c8ddf55ab917bc408d09bfa6170b16ee
fewle <erik@prof-it.se>
2015-10-27 11:53:57 +0100

    Create TicketeerSettings.user.js

---
+++ TicketeerSettings.user.js
@@ -0,0 +1,12 @@
+/ ==UserScript==
+// @name         TicketeerSettings
+// @namespace    http://your.homepage/
+// @version      0.1
+// @description  enter something useful
+// @author       You
+// @include      https://access.istone.se*
+// ==/UserScript==
+
+localStorage.setItem('ticketeerApiKey', '');
+localStorage.setItem('ticketeerUser', '');
+localStorage.setItem('ticketeerPassword', '');
